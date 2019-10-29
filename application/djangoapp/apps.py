c3a8ccbb47bed3e1dda768034bfa3cf776d0c124
Louis Bonnafous <bonnafous.louis.epita@gmail.com>
2019-10-29 20:20:40 +0100

    Fixed apps.py to register only once

--- application/djangoapp/apps.py
+++ application/djangoapp/apps.py
@@ -1,5 +1,15 @@
+import os
+
 from django.apps import AppConfig
+from apipkg import api_manager as api
+
+myappurl = "http://localhost:" + os.environ["WEBSERVER_PORT"]
 
 
 class ApplicationConfig(AppConfig):
     name = 'application.djangoapp'
+
+    def ready(self):
+        if os.environ.get('RUN_MAIN'):
+            api.unregister(os.environ['DJANGO_APP_NAME'])
+            api.register(myappurl, os.environ['DJANGO_APP_NAME'])
